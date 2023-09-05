db8132154a711f20358c502db2e06c40364a8d12
Jose Rapado <jose_manuel_rapado_garcia@carrefour.com>
2023-09-05 19:30:45 +0200

    + first structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='data_quality_report',
+      version='0.0.1',
+      description='A sample of PySpark data_quality_report application',
+      url='https://github.com/dorapa/data_quality_report',
+      author='Jose Rapado',
+      author_email='rapado.josemanuel@gmail.com',
+      packages=['data_quality_report'],
+      zip_safe=False)
